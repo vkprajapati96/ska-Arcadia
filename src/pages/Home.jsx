09702329d83c1266695176eb88b49a7cd9711d95
vkprajapati96 <vkumars426675@gmail.com>
2026-01-02 17:45:59 +0530

    Add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -119,7 +119,7 @@ const Home = () => {
               hidden md:block
               absolute top-1/2 left-5 -translate-y-1/2 
               w-[60%]
-              bg-gray-300 p-8 rounded-lg shadow-xl
+              bg-white p-8 rounded-lg shadow-xl
             "
             >
               <Content setOpenForm={setOpenForm} />
